6fae2df524d69b4b54fef94b103b58a0f4dae003
Emiliano <emilianoaloi.dev@gmail.com>
2025-07-01 21:51:58 -0300

    login directo a proveedores

--- src/page/Proveedores/ProveedoresPage.jsx
+++ src/page/Proveedores/ProveedoresPage.jsx
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
 import ProveedorsContainer from "@/components/proveedores/ProveedorContainer";
 
 const ProveedoresPage = () => {
   console.log("Renderizando ProveedoresPage!");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      // Autentica al usuario con el token (puedes almacenar la información del usuario en el estado global)
+      authenticateUser(token);
+    } else {
+      // Si no hay token, redirige al login
+      window.location.href = "/login";
+    }
+  }, []);
+
+  const authenticateUser = (token) => {
+    // Aquí puedes hacer algo con el token, como:
+    // - Guardarlo en el estado global
+    // - Realizar una validación adicional
+    // - Redirigir al usuario si es necesario
+    console.log("Token de autenticación recibido:", token);
+    // Ejemplo: podrías hacer una llamada a tu API para verificar el token
+  };
+
   return <ProveedorsContainer />;
 };
 
