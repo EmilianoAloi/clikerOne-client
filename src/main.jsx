cbfc696a0b3629ad3c0a0214540fd173b98716b3
Emiliano <emilianoaloi.dev@gmail.com>
2025-07-25 00:58:46 -0300

    forced colors update

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 const queryClient = new QueryClient();
-
+document.body.classList.add("forced-color-adjust-none");
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
