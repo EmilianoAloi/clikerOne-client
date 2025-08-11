0e556d75b051daec69eade2367924d4791f7d98e
Emiliano <emilianoaloi.dev@gmail.com>
2025-08-11 14:12:01 -0300

    .envs nuevos

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,26 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
+// Fix para __dirname en ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.VITE_BASE || "/",
+    resolve: {
+      alias: { "@": path.resolve(__dirname, "./src") },
     },
-  },
-  test: {
-    environment: "jsdom", // Para testear componentes React que usan DOM
-    globals: true,
-    setupFiles: "./src/testing/setupTests.cjs", // Opcional, para utilidades globales de test
-  },
+    test: {
+      environment: "jsdom",
+      globals: true,
+      setupFiles: "./src/testing/setupTests.cjs",
+    },
+  };
 });
